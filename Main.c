4db744041d937ba46de34cdd5a649de9c33b4196
Joan Andrés <andres6936@live.com>
2019-02-14 12:58:35 -0500

    Has been update method for load map (o dungeon)

--- Main.c
+++ Main.c
@@ -11,7 +11,6 @@
 #include "Source/Include/EDirection.h"
 #include "Source/Include/DrawDungeon.h"
 
-const int BUFFER_MINIMUN_SIZE = 10;
 const unsigned int walks = 1000;
 const unsigned int steps = 2500;
 
@@ -26,106 +25,39 @@ World loadWorld( const char *filename )
 {
     FILE *stream = fopen( filename, "r" );
 
-    if ( !stream )
+    if ( stream == NULL )
     {
-        perror( "Error while reading file" );
+        puts( "Error while reading (open) file" );
         exit( EXIT_FAILURE );
     }
 
-    char *buf = malloc( sizeof( char ) * BUFFER_MINIMUN_SIZE );
+    World newWorld;
 
-    int bufferSize = BUFFER_MINIMUN_SIZE;
+    // NOTE: Deleteme
+    newWorld.width = 1000;
+    newWorld.height = 1000;
 
-    int firstLineLength = 0;
-    int currentLineLength = 0;
+    int character;
+    int row = 0;
+    int column = 0;
 
-    _Bool firstLine = true;
-    int line_number = 0;
-
-    World new_world;
-
-    _Bool playerAlreadyFound = false;
-
-    int c;
-
-    while (( c = getc( stream )) != EOF)
+    while (( character = fgetc( stream )) != EOF)
     {
-        if ( c == '\n' )
+        if ( character == '\n' )
         {
-            if ( firstLine )
-            {
-                firstLineLength = currentLineLength;
-                new_world.width = firstLineLength;
-                firstLine = false;
-            }
-            else
-            {
-                if ( currentLineLength != firstLineLength )
-                {
-                    fprintf( stderr, "Error: map is not square\n" );
-                    exit( EXIT_FAILURE );
-                }
-            }
-//            new_world.map = realloc( ew_world.map, sizeof( char * ) * ( line_number + 1 ));
-//            new_world.map[ line_number ] = malloc( sizeof( char ) * currentLineLength );
-            memcpy( new_world.map[ line_number ], buf, currentLineLength + 1 );
-            line_number++;
-            currentLineLength = 0;
-            free( buf );
-            buf = malloc( sizeof( char ) * BUFFER_MINIMUN_SIZE );
-            bufferSize = BUFFER_MINIMUN_SIZE;
-            continue;
+            row++;
+            column = 0;
         }
-        else if (( c != '#' ) && ( c != '.' ) && ( c != '@' ))
+        else
         {
-            fprintf( stderr,
-                     "Unknown character \"%c\" in the data file \"%s\"\n at line %zu, column %zu\n",
-                     c, filename, line_number + 1, currentLineLength + 1 );
-            exit( EXIT_FAILURE );
+            newWorld.map[row][column] = (char) character;
+            column++;
         }
-        else if ( c == '@' )
-        {
-            if ( playerAlreadyFound )
-            {
-                fprintf( stderr, "More than one player position found\n" );
-                exit( EXIT_FAILURE );
-            }
-            new_world.x_pos = currentLineLength;
-            new_world.y_pos = line_number;
-            c = '.';
-            playerAlreadyFound = true;
-        }
-        else if ( bufferSize <= currentLineLength )
-        {
-            buf = realloc( buf, ( bufferSize + BUFFER_MINIMUN_SIZE ) * sizeof( char ));
-            bufferSize += BUFFER_MINIMUN_SIZE;
-        }
-        buf[ currentLineLength++ ] = c;
-    }
-
-    free( buf );
-
-    if ( !playerAlreadyFound )
-    {
-        fprintf( stderr, "No player was found on the map\n" );
-        exit( EXIT_FAILURE );
-    }
-
-    new_world.height = line_number;
-
-    if ( !feof( stream ))
-    {
-        perror( "Error while reading file" );
-        exit( EXIT_FAILURE );
     }
 
     fclose( stream );
 
-#if DEBUG
-    puts("The map has been loaded sucessfully.");
-#endif
-
-    return new_world;
+    return newWorld;
 }
 
 World newWorld( int height, int width, int walks, int steps, Player* player )
@@ -213,26 +145,20 @@ World newWorld( int height, int width, int walks, int steps, Player* player )
 
 int main( int argc, char *argv[] )
 {
-    if ( argc > 2 )
-    {
-        usage( );
-        exit( EXIT_FAILURE );
-    }
-    if (( argc == 2 ) && ( !strcmp( argv[ 1 ], "-h" ) || !strcmp( argv[ 1 ], "--help" )))
-    {
-        usage( );
-        exit( EXIT_SUCCESS );
-    }
-
     World world;
     Player player;
 
+    // We assume that the second parameter points to a file that contains
+    // a map already generated.
     if ( argc == 2 )
     {
         world = loadWorld( argv[ 1 ] );
+        player.x = 60;
+        player.y = 30;
     }
     else
     {
+        // If there are not argument, generate a new map.
         world = newWorld( MAP_HEIGHT, MAP_WIDTH, walks, steps, &player );
     }
 
