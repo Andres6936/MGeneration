b871ab4eb883830d131d3a6afa4e0a3116c6ddfb
Joan Andrés <andres6936@live.com>
2020-05-16 17:34:14 -0500

    Added new enum for control the scene showed

---
+++ Include/Scene/NextScene.hpp
@@ -0,0 +1,18 @@
+// Design by Joan Andrés.
+
+#ifndef MGENERATION_NEXTSCENE_HPP
+#define MGENERATION_NEXTSCENE_HPP
+
+namespace Gen
+{
+
+	enum class NextScene
+	{
+		Menu,
+		Play,
+		Stop,
+	};
+
+}
+
+#endif //MGENERATION_NEXTSCENE_HPP
